bc5fbeb4098ee5b2101e10470f8d62c0dc7b78ce
gurvesh <guru01@gmail.com>
2023-03-20 11:14:51 +0530

    1.5 - Added a key attribute to avoid error

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -13,18 +13,23 @@ const Part = (props) => {
 const Content = props => {
   return (
     <>
-      {props.parts.map(part => <Part part={part.name} exercises={part.exercises} />)}
+      {props.parts.map(part => 
+        // We need to add a key prop to avoid an error. See this page: https://react.dev/learn/rendering-lists#keeping-list-items-in-order-with-key
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      )}
     </>
   )
 }
 
 const Total = (props) => {
   const exercises = props.parts.map(part => part.exercises)
-  const totalExercises = exercises.reduce(function(x, y) {
-    return x + y;
-  }, 0);
+  let sum = 0;
+  for (const n of exercises) { sum += n }
+  // const totalExercises = exercises.reduce(function(x, y) {
+  //   return x + y;
+  // }, 0);
   return (
-    <p>Number of exercises {totalExercises}</p>
+    <p>Number of exercises {sum}</p>
   )
 }
 
