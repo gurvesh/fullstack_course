0fb4eb78432b0d3e7b59c6bd021129125dfbf653
gurvesh <guru01@gmail.com>
2023-03-22 11:16:19 +0530

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteringString, setNewFilteringString] = useState('')
+
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:4000/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -36,7 +47,7 @@ const App = () => {
       .toLowerCase()
       .includes(
         filteringString.toLowerCase()
-    )
+      )
   )
 
   const handleNewName = (event) => {
